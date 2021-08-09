618bd85768808e2efa14b114a8a773df98d2df71
Cullen S <cullensmi@gmail.com>
2021-08-08 22:45:57 -0400

    added the problem in comments

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -1,8 +1,43 @@
 package com.csmithswim;
+import java.util.*;
+
+
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+/*        Ask a user to enter a natural number.
+        If the number is not natural, print an error message.
+        Calculate and print the parity of the number.
+        Check whether is the number is a Buzz number and print the explanation.
+        Finish the program after printing the message.
+*/
+
+
+        System.out.println("Enter a natural number: ");
+
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+
+        if (input % 2 == 0) {
+            System.out.println("This number is Even.");
+        } else {
+            System.out.println("This number is Odd.");
+        }
+
+
+        if (input % 7 == 0 || (input - 7) % 2 ==0) {
+            System.out.println("It is a Buzz number.");
+        }
+        System.out.println("Explanation:");
+
+        if (input % 7 == 0 && (input - 7 % 2 == 0)) {
+            System.out.println(input + " is divisible by 7 and ends with 7.");
+        } else if (input % 7 == 0) {
+            System.out.println(input + " is divisible by 7.");
+        } else if ((input - 7 % 2 == 0)) {
+            System.out.println(input + " ends with 7");
+        }
+
     }
 }
