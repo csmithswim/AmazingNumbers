a30237c9a511f948632432f30186738e62996c14
Cullen S <cullensmi@gmail.com>
2021-08-09 20:48:31 -0400

    solved first buzz challenge

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -12,32 +12,47 @@ public class Main {
         Check whether is the number is a Buzz number and print the explanation.
         Finish the program after printing the message.
 */
+        boolean program = true;
+
+        while (program) {
+            System.out.println("Enter a natural number: ");
+            Scanner scanner = new Scanner(System.in);
+            int input = scanner.nextInt();
+            boolean buzz = true;
 
 
-        System.out.println("Enter a natural number: ");
+            if (input <= 0) {
+                System.out.println("This number is not natural!");
+                break;
+            }
+            if (input % 2 == 0) {
+                System.out.println("This number is Even.");
+            } else {
+                System.out.println("This number is Odd.");
+            }
 
-        Scanner scanner = new Scanner(System.in);
-        int input = scanner.nextInt();
+            if (input % 7 == 0 || (input - 7) % 10 == 0) {
+                System.out.println("It is a Buzz number.");
+            } else {
+                System.out.println("It is not a Buzz number.");
+                buzz = false;
+            }
 
-        if (input % 2 == 0) {
-            System.out.println("This number is Even.");
-        } else {
-            System.out.println("This number is Odd.");
+            if (buzz) {
+                System.out.println("buzz");
+                if (input % 7 == 0 && (input - 7) % 10 == 0) {
+                    System.out.println(input + " is divisible by 7 and ends with 7.");
+                    break;
+                } else if (input % 7 == 0) {
+                    System.out.println(input + " is divisible by 7.");
+                    break;
+                } else if ((input - 7) % 10 == 0) {
+                    System.out.println(input + " ends with 7.");
+                    break;
+                }
+            } else {
+                System.out.println(input + " is neither divisible by 7 nor does it end with 7.");
+            }
         }
-
-
-        if (input % 7 == 0 || (input - 7) % 2 ==0) {
-            System.out.println("It is a Buzz number.");
-        }
-        System.out.println("Explanation:");
-
-        if (input % 7 == 0 && (input - 7 % 2 == 0)) {
-            System.out.println(input + " is divisible by 7 and ends with 7.");
-        } else if (input % 7 == 0) {
-            System.out.println(input + " is divisible by 7.");
-        } else if ((input - 7 % 2 == 0)) {
-            System.out.println(input + " ends with 7");
-        }
-
     }
 }
